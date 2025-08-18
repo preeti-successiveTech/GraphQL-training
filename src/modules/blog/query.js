c1455da467669e6eac975e3b29ab8538fcc0571e
Preeti Garg <preeti.garg@preetivrs-36.successivetech.com>
2025-08-18 10:35:58 +0530

    Assignment-2 in progress

--- src/modules/blog/query.js
+++ src/modules/blog/query.js
@@ -3,22 +3,40 @@ import { posts, users } from "./dataSource.js";
 export const blogQueryResolvers = {
   getUsers: () => users,
 
-  getUserById: (_,{id}) => users.find((user) => user.id === id),
+  getUserById: async (_, { id }) => {
+    await new Promise(resolve => setTimeout(resolve, 2000)); 
+    const user = users.find((user) => user.id === id);
+    if (!user) throw new Error("User not found");
+    return user;
+  },
 
-  getPosts: () => posts,
+  getPosts: (_, { page, limit, sortOrder = "ASC" }) => {
+    if (page < 1 || limit < 1) {
+      throw new Error("Invalid page or limit");
+    }
 
-  getPostById: (_,{id}) => posts.find((post) => post.id === id),
+    const sorted = [...posts].sort((a, b) => {
+      const idA = parseInt(a.id);
+      const idB = parseInt(b.id);
+      return sortOrder === "DESC" ? idB - idA : idA - idB;
+    });
+    const skip = (page - 1) * limit;
+    return sorted.slice(skip, skip + limit);
+  },
 
-  getPostsByUserId: (_,{userId}) =>
+  getPostById: (_, { id }) => posts.find((post) => post.id === id),
+
+  getPostsByUserId: (_, { userId }) =>
     posts.filter((post) => post.authorId === userId),
 
   getComments: () => comments,
 
-  getCommentsByPostId: (_,{postId}) =>
+  getCommentsByPostId: (_, { postId }) =>
     comments.filter((comment) => comment.postId === postId),
 
-  getUserByComment: (_,{comment}) =>
+  getUserByComment: (_, { comment }) =>
     users.find((user) => user.id === comment.userId),
 
-  getAuthorOfPost: (_,{post}) => users.find((user) => user.id === post.authorId),
+  getAuthorOfPost: (_, { post }) =>
+    users.find((user) => user.id === post.authorId),
 };
